26ebeee60ca998be3df308284e5db5e233546632
TEMOA <TEMOA@DESKTOP-ABDV0A5>
2021-01-09 01:16:02 +0300

    first commit

---
+++ src/controler/UsersContoller.java
@@ -0,0 +1,48 @@
+package controler;
+
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import model.Users;
+
+public class UsersContoller  {
+
+    @FXML
+    private TableView<Users> col_table;
+    @FXML
+    private TableColumn col_userName;
+    @FXML
+    private TableColumn col_password;
+    @FXML
+    private TableColumn col_email;
+
+    @FXML
+    void onAdd(ActionEvent event) {
+    	Stage addStage = new Stage();
+    	FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource("/interfaces/SignUp.fxml"));
+    	try {
+			addStage.setScene(new Scene(loader.load()));
+		} catch (IOException e) {}
+    	addStage.setTitle("add user");
+    	addStage.show();
+    }
+
+    @FXML
+    void onDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdate(ActionEvent event) {
+
+    }
+}
